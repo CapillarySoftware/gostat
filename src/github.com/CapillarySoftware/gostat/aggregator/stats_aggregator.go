398827b566d57a8978fb270a395b5c9445748ae9
Andrew Goldstein <abgoldst@gmail.com>
2014-07-16 21:51:11 -0600

    computing average, min, max for a collection of more than one Stat

--- src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator.go
+++ src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator.go
@@ -7,9 +7,28 @@ type StatsAggregator struct {
 }
 
 func (a StatsAggregator) Aggregate(stats []stat.Stat) (average, min, max float64) {
-	if stats == nil {
+	if stats == nil || len(stats) == 0 {
 		return
 	}
+
+	min = stats[0].Value
+	max = stats[0].Value
+	sum := 0.0
+
+	for i := range stats {
+		v := stats[i].Value
+		
+		sum += v
+
+		if v < min {
+			min = v
+		}
+
+		if v > max {
+			max = v
+		}
+	}
+	average = sum / float64(len(stats))
 
 	return
 }
--- src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
+++ src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/CapillarySoftware/gostat/stat"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"time"
 )
 
 var _ = Describe("StatsAggregator", func() {
@@ -30,14 +31,29 @@ var _ = Describe("StatsAggregator", func() {
 		})
 
 		It("Should return all the same values if the slice contains just one Stat", func() {
-			stats := []stat.Stat{"foo", now(), 123.456}
+			const value = 123.456
+			stats := []stat.Stat{{"foo", time.Now().UTC(), value}}
 
-			average, min, max := sa.Aggregate([]stat.Stat{})
-			Expect(average).To(Equal(0.0))
-			Expect(min).To(Equal(0.0))
-			Expect(max).To(Equal(0.0))
+			average, min, max := sa.Aggregate(stats)
+			Expect(average).To(Equal(value))
+			Expect(min).To(Equal(value))
+			Expect(max).To(Equal(value))
 		})
 
+		It("Should return the expected values for a collection of more than one Stat", func() {
+
+			stats := []stat.Stat{{"foo", time.Now().UTC(), 1},
+			                     {"foo", time.Now().UTC(), 2},
+			                     {"foo", time.Now().UTC(), 3},
+			                     {"foo", time.Now().UTC(), 4},
+			                     {"foo", time.Now().UTC(), 5},
+			                     {"foo", time.Now().UTC(), 6}}
+
+			average, min, max := sa.Aggregate(stats)
+			Expect(average).To(Equal(3.5))
+			Expect(min).To(Equal(1.0))
+			Expect(max).To(Equal(6.0))
+		})
 	})
 
 })
