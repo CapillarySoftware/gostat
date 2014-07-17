bd0a0e2922a1b054481e50e50ded00cb59d99fcc
Andrew Goldstein <abgoldst@gmail.com>
2014-07-16 21:20:43 -0600

    refactor construction of StatsAggregator into a JustBeforeEach

--- src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
+++ src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
@@ -10,13 +10,12 @@ var _ = Describe("StatsAggregator", func() {
 
 	var sa StatsAggregator
 
-	BeforeEach(func() {
+	JustBeforeEach(func() {
 		sa = StatsAggregator{}
 	})
 
 	Describe("Aggregate", func() {
 		It("Should return all 0 values if a nil slice is received", func() {
-
 			average, min, max := sa.Aggregate(nil)
 			Expect(average).To(Equal(0.0))
 			Expect(min).To(Equal(0.0))
@@ -24,7 +23,6 @@ var _ = Describe("StatsAggregator", func() {
 		})
 
 		It("Should return all 0 values if an empty slice is received", func() {
-
 			average, min, max := sa.Aggregate([]stat.Stat{})
 			Expect(average).To(Equal(0.0))
 			Expect(min).To(Equal(0.0))
