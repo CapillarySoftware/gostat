02d09b98cf14f95242b939d397d4dcf93bf0d091
Andrew Goldstein <abgoldst@gmail.com>
2014-07-16 21:19:27 -0600

    added StatsAggregator and Aggregate tests (for nil / empty slices)

--- src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
+++ src/github.com/CapillarySoftware/gostat/aggregator/stats_aggregator_test.go
@@ -8,12 +8,14 @@ import (
 
 var _ = Describe("StatsAggregator", func() {
 
+	var sa StatsAggregator
+
 	BeforeEach(func() {
+		sa = StatsAggregator{}
 	})
 
 	Describe("Aggregate", func() {
 		It("Should return all 0 values if a nil slice is received", func() {
-			sa := StatsAggregator{}
 
 			average, min, max := sa.Aggregate(nil)
 			Expect(average).To(Equal(0.0))
@@ -22,7 +24,6 @@ var _ = Describe("StatsAggregator", func() {
 		})
 
 		It("Should return all 0 values if an empty slice is received", func() {
-			sa := StatsAggregator{}
 
 			average, min, max := sa.Aggregate([]stat.Stat{})
 			Expect(average).To(Equal(0.0))
