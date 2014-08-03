dd3001a3e76ce8ef5b8037a4887ccfd919263ab1
Andrew Goldstein <abgoldst@gmail.com>
2014-08-02 23:04:27 -0600

    implemented insert()

--- src/github.com/CapillarySoftware/gostat/bucketer/bucketer.go
+++ src/github.com/CapillarySoftware/gostat/bucketer/bucketer.go
@@ -3,32 +3,56 @@ package bucketer
 import (
 	"github.com/CapillarySoftware/gostat/stat"
 	"time"
+	"fmt"
 )
 
 type Bucketer struct {
 	currentBucketMinTime  time.Time
-	currentBuckets        map[string][]stat.Stat
+	currentBuckets        map[string][]*stat.Stat
 		
 	previousBucketMinTime time.Time
-	previousBuckets       map[string][]stat.Stat
+	previousBuckets       map[string][]*stat.Stat
 
 	input                 <-chan *stat.Stat
-	output                chan<- []stat.Stat
+	output                chan<- []*stat.Stat
 	shutdown              <-chan bool
 }
 
 //cs chan string
-func NewBucketer(stats <-chan *stat.Stat, bucketedStats chan<- []stat.Stat, shutdown <-chan bool) *Bucketer {
+func NewBucketer(stats <-chan *stat.Stat, bucketedStats chan<- []*stat.Stat, shutdown <-chan bool) *Bucketer {
 	startOfCurrentMin := time.Now().UTC().Truncate(time.Minute) // "now", rounded down to the current min
 
 	return &Bucketer {
 		currentBucketMinTime  : startOfCurrentMin,
-		currentBuckets        : make(map[string][]stat.Stat),
+		currentBuckets        : make(map[string][]*stat.Stat),
 		previousBucketMinTime : startOfCurrentMin.Add(time.Minute * -1), // one minute behind
-		previousBuckets       : make(map[string][]stat.Stat),
+		previousBuckets       : make(map[string][]*stat.Stat),
 
 		input                 : stats,
 		output                : bucketedStats,
 		shutdown              : shutdown,
 	}
+}
+
+// BucketStat places the provided stat in the appropriate bucket
+// It returns an error if the stat could not be placed in a bucket
+func (b Bucketer) insert(s *stat.Stat) error {
+	var buckets map[string][]*stat.Stat
+
+	if s == nil {
+		return fmt.Errorf("dropping nil stat")
+	}
+
+	if s.Timestamp.After(b.currentBucketMinTime) ||  s.Timestamp.Equal(b.currentBucketMinTime) {
+		buckets = b.currentBuckets
+	} else if s.Timestamp.After(b.previousBucketMinTime) || s.Timestamp.Equal(b.previousBucketMinTime) {
+		buckets = b.previousBuckets
+	} else {
+		return fmt.Errorf("Bucketer: dropping stat older than %v: %+v", b.previousBucketMinTime, *s)
+	}
+
+	stats := buckets[s.Name]
+	stats = append(stats, s)
+	buckets[s.Name] = stats
+	return nil
 }
--- src/github.com/CapillarySoftware/gostat/bucketer/bucketer_test.go
+++ src/github.com/CapillarySoftware/gostat/bucketer/bucketer_test.go
@@ -4,18 +4,19 @@ import (
   "github.com/CapillarySoftware/gostat/stat"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"fmt"
 	"time"
 )
 
 var _ = Describe("Bucketer", func() {
 
 	var stats         <-chan *stat.Stat
-	var bucketedStats chan<- []stat.Stat
+	var bucketedStats chan<- []*stat.Stat
 	var shutdown      <-chan bool
 
 	JustBeforeEach(func() {
 		stats         = make(chan *stat.Stat)
-		bucketedStats = make(chan []stat.Stat)
+		bucketedStats = make(chan []*stat.Stat)
 		shutdown      = make(chan bool)
 	})
 
@@ -23,7 +24,7 @@ var _ = Describe("Bucketer", func() {
 		It("should return a properly initialized Bucketer", func() {
 			x := NewBucketer(stats, bucketedStats, shutdown)
 
-			// a newly constructed StatBucketer has nothing in it's buckets
+			// a newly constructed Bucketer has nothing in it's buckets
 			Expect(len(x.currentBuckets)).To(Equal(0))
 			Expect(len(x.previousBuckets)).To(Equal(0))
 
@@ -36,4 +37,87 @@ var _ = Describe("Bucketer", func() {
 			Expect(x.currentBucketMinTime.Sub(x.previousBucketMinTime)).To(Equal(time.Duration(time.Minute)))
 		})
 	})
+
+	Describe("Insert", func() {
+		It("should return an error if a nil stat is inserted", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			Expect(x.insert(nil)).To(Equal(fmt.Errorf("dropping nil stat")))
+		})
+
+		It("should insert the stat in the current bucket if the Timestamp is after the current bucket's min time", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			s := stat.Stat{Name : "foo", Timestamp : x.currentBucketMinTime.Add(time.Duration(time.Second)), Value : 1}
+			Expect(s.Timestamp).To(BeTemporally(">", x.currentBucketMinTime))
+			Expect(s.Timestamp).To(BeTemporally(">", x.previousBucketMinTime))
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+			
+			Expect(x.insert(&s)).To(BeNil())
+			
+			Expect(x.currentBuckets[s.Name][0]).To(Equal(&s))
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+		})
+
+		It("should insert the stat in the current bucket if the Timestamp is the same as the current bucket's min time", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			s := stat.Stat{Name : "foo", Timestamp : x.currentBucketMinTime, Value : 1}
+			Expect(s.Timestamp).To(BeTemporally("==", x.currentBucketMinTime))
+			Expect(s.Timestamp).To(BeTemporally(">",  x.previousBucketMinTime))
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+			
+			Expect(x.insert(&s)).To(BeNil())
+			
+			Expect(x.currentBuckets[s.Name][0]).To(Equal(&s))
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+		})
+
+		It("should insert the stat in the previous bucket if the Timestamp is less than the current bucket's min time and greater than the previous bucket's min time", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			s := stat.Stat{Name : "foo", Timestamp : x.currentBucketMinTime.Add(time.Duration(time.Second * -1)), Value : 1}
+			Expect(s.Timestamp).To(BeTemporally("<", x.currentBucketMinTime))
+			Expect(s.Timestamp).To(BeTemporally(">", x.previousBucketMinTime))
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+			
+			Expect(x.insert(&s)).To(BeNil())
+			
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name][0]).To(Equal(&s))
+		})
+
+		It("should insert the stat in the previous bucket if the Timestamp is less than the current bucket's min time and equal to the previous bucket's min time", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			s := stat.Stat{Name : "foo", Timestamp : x.previousBucketMinTime, Value : 1}
+			Expect(s.Timestamp).To(BeTemporally("<", x.currentBucketMinTime))
+			Expect(s.Timestamp).To(BeTemporally("==", x.previousBucketMinTime))
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+			
+			Expect(x.insert(&s)).To(BeNil())
+			
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name][0]).To(Equal(&s))
+		})
+
+		It("should NOT insert the stat if the Timestamp is less than the previous bucket's min time", func() {
+			x := NewBucketer(stats, bucketedStats, shutdown)
+
+			s := stat.Stat{Name : "foo", Timestamp : x.previousBucketMinTime.Add(time.Duration(time.Second * -1)), Value : 1}
+			Expect(s.Timestamp).To(BeTemporally("<", x.currentBucketMinTime))
+			Expect(s.Timestamp).To(BeTemporally("<", x.previousBucketMinTime))
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+			
+			Expect(x.insert(&s)).To(Equal(fmt.Errorf("Bucketer: dropping stat older than %v: %+v", x.previousBucketMinTime, s)))
+			
+			Expect(x.currentBuckets[s.Name]).To(BeNil())
+			Expect(x.previousBuckets[s.Name]).To(BeNil())
+		})
+	})	
 })
