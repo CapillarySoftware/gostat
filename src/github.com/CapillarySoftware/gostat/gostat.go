467414ba25b22d02e72f474fd4d8aca3cf813547
Andrew Goldstein <abgoldst@gmail.com>
2014-08-04 01:32:49 -0600

    wired in nanomsg pull socket

--- src/github.com/CapillarySoftware/gostat/gostat.go
+++ src/github.com/CapillarySoftware/gostat/gostat.go
@@ -8,20 +8,26 @@ import (
 	"os"
 	"os/signal"
 	"math/rand"
+	nano "github.com/op/go-nanomsg"
+	log  "github.com/cihub/seelog"
 )
 
 func main () {
 	stats            := make(chan *stat.Stat)   // stats received from producers
 	bucketedStats    := make(chan []*stat.Stat) // raw bucketed (non-aggregated) stats are output here
 	shutdownBucketer := make(chan bool)         // used to signal to the bucketer we are done
+	shutdownListener := make(chan bool)         // used to signal to the socket listener we are done
 
-  installCtrlCHandler(&shutdownBucketer)
+  installCtrlCHandler(shutdownBucketer, shutdownListener)
 
 	// create and start a Bucketer
 	b := bucketer.NewBucketer(stats, bucketedStats, shutdownBucketer)
 	go b.Run()
 
-	// TODO: replace this simulation of stats with (something like) a 0mq receiver
+	// start a socket listener
+	go bindSocketListener(stats, shutdownListener)
+
+	// TODO: remove this simulation of stats when the socket listener is really processing data
 	for true {
 		<-time.After(time.Second * time.Duration(rand.Intn(3))) // sleep 0-3 seconds
 
@@ -33,17 +39,63 @@ func main () {
 
 // installCtrlCHandler starts a goroutine that will signal the workers when it's time
 // to shut down
-func installCtrlCHandler(shutdownBucketer *chan bool) {
+func installCtrlCHandler(shutdown ...chan<- bool) {
 	c := make(chan os.Signal, 1)                                       
 	signal.Notify(c, os.Interrupt)                                     
 
 	go func() {                                                        
 	  for sig := range c {                                             
 	    fmt.Printf(" captured %v, stopping stats collection and exiting...\n", sig)
-	    *shutdownBucketer <-true  // stop the bucketer
+	    for _, s := range shutdown {
+	    	s <-true
+	    }
 	    <-time.After(time.Second * 5) // wait for a clean shutdown, TODO: wait on signal from all routines
 	    fmt.Println("Done")                                      
 	    os.Exit(1)                                                     
 	  }                                                                
 	}()
 }
+
+
+func bindSocketListener(stats chan<- *stat.Stat, shutdown <-chan bool) {
+	var (
+		msg []byte
+		err error
+		done = false
+	)
+	socket, err := nano.NewPullSocket()
+
+	if nil != err {
+		log.Error(err)
+	}
+
+	socket.SetRecvTimeout(time.Second)
+
+	defer socket.Close()
+	_, err = socket.Bind("tcp://*:2025")
+	if nil != err {
+		log.Error(err)
+	}
+	log.Info("Ready to receive data")
+
+	for !done {
+		msg, err = socket.Recv(0) //blocking
+		if nil != err {
+			log.Error(err)
+		}
+		if nil != msg {
+			// TODO: new up a Stat and send it on the stats channel
+			log.Debug("Received message: ", msg)
+		}
+
+		select {
+			case done = <- shutdown : break
+			default                 : break
+		}
+	}
+
+	fmt.Println("Exiting socket listener")
+}
+
+
+
